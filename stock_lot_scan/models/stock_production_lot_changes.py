cadd20ffcd3b069b3672bc7d5f68eb67e1e4a6fa
Ahmed Gaber <ahmedgaber2050@yahoo.com>
2021-07-03 22:20:52 +0300

    fix stock_lot scan

--- stock_lot_scan/models/stock_production_lot_changes.py
+++ stock_lot_scan/models/stock_production_lot_changes.py
@@ -7,7 +7,7 @@ from odoo import models
 class stock_production_lot_inherit(models.Model):
     _inherit = 'stock.production.lot'
 
-    @api.onchange('expiration_date')
-    def get_exp_date(self):
-        date = self.expiration_date
-        self.life_date = date
+    #@api.onchange('expiration_date')
+    #def get_exp_date(self):
+    #    date = self.expiration_date
+    #    self.life_date = date
